280f3ff744025f33f4bfa5d2a53919aecd1225ab
kamildev7 <k.93kamil@gmail.com>
2022-05-31 18:02:53 +0200

    Added verification in controller

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -1,8 +1,12 @@
 package guru.sfg.brewery.web.controllers;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +21,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
     private final UserRepository userRepository;
+    private final GoogleAuthenticator googleAuthenticator;
 
     @GetMapping("/register2fa")
     public String register2fa(Model model) {
-        model.addAttribute("googleurl", "todo");
+
+        User user = getUser();
+        String url = GoogleAuthenticatorQRGenerator.getOtpAuthURL("SFG", user.getUsername(),
+                googleAuthenticator.createCredentials(user.getUsername()));
+
+        log.debug("Google QR URL: " + url);
+
+        model.addAttribute("googleurl", url);
 
         return "user/register2fa";
     }
@@ -28,8 +40,23 @@ public class UserController {
     @PostMapping
     public String confirm2fa(@RequestParam Integer verifyCode) {
 
-        //todo
+        User user = getUser();
+        log.debug("Entered Code is:" + verifyCode);
 
-        return "index";
+        if (googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)) {
+            User savedUser = userRepository.findById(user.getId()).orElseThrow();
+            savedUser.setUserGoogle2fa(true);
+            userRepository.save(savedUser);
+
+            return "/index";
+        } else {
+            //bad code
+            return "user/register2fa";
+        }
+
+    }
+
+    private User getUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
